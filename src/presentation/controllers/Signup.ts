de2eefdc2052e80d4bc9c81bc84d5bb3d9d5f9e5
odevpedro <pedrosschmidt2@gmail.com>
2023-11-15 14:57:04 -0300

    chore: add .gitignore

--- src/presentation/controllers/Signup.ts
+++ src/presentation/controllers/Signup.ts
@@ -1,19 +1,14 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
-import { HttpResponse, HttpRequest } from '../protocols/htpp'
+import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/htttp-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     } else if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     } else {
       // Adiciona um retorno final
       return {
